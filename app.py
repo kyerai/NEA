da21636b8e3dcf022effc6e94a0fa95e338a3551
kyerai <160567572+kyerai@users.noreply.github.com>
2024-05-22 09:13:02 +0400

    fixed the authentication system so now all sessions worked, it is now finished.

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, redirect, url_for
-from flask import request
+from flask import Flask, render_template, redirect, url_for, request, session
 import sqlite3
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'  # Set a secret key for sessions
 
 @app.route('/')
+@app.route('/index')
 def index():
     return render_template('index.html')
 
@@ -30,36 +31,38 @@ def signup():
         
     return render_template('signup.html')
 
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        connect = sqlite3.connect('/Users/kyeraikundalia/Documents/GitHub/NEA/users.db')
-        cursor = connect.cursor()
-
         username = request.form['username']
         password = request.form['password']
-        cursor.execute('''
-            SELECT * FROM users
-            WHERE username = ? AND password = ?
-        ''', (username, password))
+        
+        conn = sqlite3.connect('/Users/kyeraikundalia/Documents/GitHub/NEA/users.db')
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM users WHERE username = ? AND password = ?", (username, password))
         user = cursor.fetchone()
 
         if user:
-            return render_template("home.html", _methods=["POST"])  # Redirect to the home page with POST method
+            session['logged_in'] = True  # Set session variable
+            session['username'] = username  # Store username in session
+            return redirect(url_for('home'))
         else:
             return 'Username or password is incorrect'
 
     return render_template('login.html')
 
-@app.route('/home', methods=['POST', 'GET'])
+@app.route('/home')
 def home():
-    if request.method == 'POST':
+    if 'logged_in' in session and session['logged_in']:
         return render_template('home.html')
-    elif request.method == 'GET':
-        return render_template("login.html", error="Please login first")
+    else:
+        return redirect(url_for('login'))
 
-
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)  # Remove session variable
+    session.pop('username', None)  # Remove username from session
+    return redirect(url_for('index'))  # Redirect to index page
 
 if __name__ == '__main__':
-    app.run(host=0.0, port=4000, debug=True)
+    app.run(debug=True)

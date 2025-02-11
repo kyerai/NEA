b16b1b5ef1d26716e70c3257817ac958bf653380
kyerai <160567572+kyerai@users.noreply.github.com>
2025-02-11 10:15:58 +0400

    created requirements

--- wsgi.py
+++ wsgi.py
@@ -8,5 +8,5 @@ stock_app.register_routes()  # Ensure routes are registered
 app = stock_app.app  # Access the Flask instance
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 4000))  # Default to 5000
+    port = int(os.environ.get("PORT", 4000))  
     app.run(host="0.0.0.0", port=port)
